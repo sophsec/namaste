975e0f75bf60318f292e8ae5a2be8432b64add6d
postmodern <postmodern.mod3@gmail.com>
2009-04-17 00:33:48 -0700

    Added some basic controller code.

--- app.rb
+++ app.rb
@@ -13,19 +13,25 @@ get '/note/new' do
 end
 
 post '/note/create' do
+  @note = Note.create(params[:note])
 end
 
 get '/note/:id' do
+  @note = Note.first(:id => params[:id])
 end
 
 get '/note/' do
+  @notes = Note.all
 end
 
 post '/comment/create' do
+  @comment = Comment.create(params[:comment])
 end
 
 get '/comment/:id' do
+  @comment = Comment.first(:id => params[:id])
 end
 
 get '/comment/' do
+  @comments = Comment.all
 end

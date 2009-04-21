cd114e6db222f16101c783c64515646a3bda9830
postmodern <postmodern.mod3@gmail.com>
2009-04-20 19:55:53 -0700

    Cleaned up the textile syntax table.

--- public/scripts/textile.js
+++ public/scripts/textile.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
   $("a.show_syntax").click(function() {
-    $("#textile_syntax").slideToggle();
+    $("#textile_syntax").slideToggle("normal", function() {
+      $("#textile_syntax").focus();
+    });
   });
+
+  $("#textile_syntax").hide();
 });
